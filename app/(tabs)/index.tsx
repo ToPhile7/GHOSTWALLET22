960a282cac133277a8037a05fb6628f29c4fd53e
ToPhile7 <89874800+ToPhile7@users.noreply.github.com>
2025-10-01 14:46:49 +0200

    Updated index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -180,7 +180,7 @@ const styles = StyleSheet.create({
   },
   titleContainer: {
     alignItems: 'center',
-    marginTop: -20,
+    marginTop: -200,
     justifyContent: 'center',
   },
   titleImage: {
